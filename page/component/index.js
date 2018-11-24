14d910518baef1b06a36b0af7f83f8ec008bb39f
Big Gang <15617527@qq.com>
2018-11-24 23:11:35 +0800

    mock index latest activity list

--- page/component/index.js
+++ page/component/index.js
@@ -9,5 +9,27 @@ Page({
     autoplay: false,
     interval: 3000,
     duration: 800,
+    latestActivity:[
+      {
+        "subject":"乐高玩具开发动手能力和儿童智力",
+        "sponsor":"优学联盟",
+        "date":"2018.9.27"
+      }, 
+      {
+        "subject": "速记古诗词",
+        "sponsor": "优学联盟",
+        "date": "2018.9.27"
+      },
+      {
+        "subject": "乐高玩具开发动手能力和儿童智力",
+        "sponsor": "优学联盟",
+        "date": "2018.9.27"
+      },
+      {
+        "subject": "乐高玩具开发智力",
+        "sponsor": "优学联盟",
+        "date": "2018.9.27"
+      }
+    ]
   }
 })
